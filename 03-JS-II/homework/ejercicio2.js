dda9d8c513a43d06f677c150016a435616dd62e6
Juan Pablo Carrasco Borrero <juanpablocarrascoborrero@MacBook-de-Juan.local>
2021-12-01 08:12:33 -0500

    ejercicio 2 realizado

---
+++ 03-JS-II/homework/ejercicio2.js
@@ -0,0 +1,35 @@
+
+"for"
+"la instruccion" 
+"for" 
+"sirve para crear bucles y que"
+ "consta de tres expresiones encerradas y separadas cada una de un "
+ "punto y coma, y seguido a  estas expresiones se encuentra una instruccion que se ejecutara en bucle"
+ 
+ 
+ for ([initialization]; [condition]; [final-expression]) {
+      statement
+ }
+   
+"&&"
+"Operador logico AND"
+
+"para un conjunto de operandos es verdadero si y solo si todos sus operandos son verdaderos"
+"delvera verdadero si ambas expresiones son "
+'true' 
+"si alguna expresion es false devolvera false "
+
+
+ " || "
+
+"Operador logico OR "
+"devolvera True si alguna de la dos expresiones es true "
+"devolvera false si ambas expresiones son false"
+
+
+"! " 
+
+"Operador logico NOT"
+"el operador NOT devolvera el valor Booleano Opuesto"
+
+
